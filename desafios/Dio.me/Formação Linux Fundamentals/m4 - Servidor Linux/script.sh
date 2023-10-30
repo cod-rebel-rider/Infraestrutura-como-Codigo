f296f5170784d575ff1a096db04ad5f961f8ba20
Matheus Rodrigues <91558025+cod-rebel-rider@users.noreply.github.com>
2023-10-30 13:45:50 -0300

    Função Verificar e Instalar Pacotes

--- desafios/Dio.me/Formação Linux Fundamentals/m4 - Servidor Linux/script.sh
+++ desafios/Dio.me/Formação Linux Fundamentals/m4 - Servidor Linux/script.sh
@@ -25,3 +25,24 @@ echo "Atalização concluida!"
 echo ""
 echo "========================================"
 echo ""
+
+#Instalando pacotes necessários
+pacotes=("apache2" "unzip" "wget")
+
+echo "Instalando pacotes necessários...."
+
+for pacote in "${pacotes[@]}"; do
+    if dpkg -l | grep -q $pacote; then
+        echo ""
+        echo "$pacote já está instalado."
+        echo ""
+    else
+        echo ""
+        echo "$pacote não foi encontrado e precisa ser instalado."
+        echo "Iniciando instalação..."
+        echo ""
+        sudo apt install $pacote -y
+    fi
+    echo ""
+done
+
